c192b9cb32861bb0eace8a7ed9f9f81eb2f47688
siyeon <72018558+siyeon0227@users.noreply.github.com>
2020-10-17 14:20:37 +0900

    Update input_page.dart

--- input_page.dart
+++ input_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:project_week1_1/screens/reference_page.dart';
+import 'package:project_week1_1/screens/history_page.dart';
 
 class InputPage extends StatefulWidget {
   @override
@@ -15,32 +18,178 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('test'),
-          ),
           body: MyPage(),
       ),
     );
   }
 }
 
+
 class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
+    return Scaffold(
+        backgroundColor: Color.fromRGBO(247, 247,247,1),
 
-       TextField(
+    body: Center(
 
-         decoration: InputDecoration(
-           border: InputBorder.none,
-           hintText: 'Enter Korean text'
-         ),
+    child: SingleChildScrollView(
+    child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
 
-       )
+          children: <Widget>[
 
-      ],
+            Center(
+              child: Container(
 
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+
+
+                    FlatButton(
+                        child:  Image(
+                          image:AssetImage('image/5-layers.png'),
+                          width:40,
+                          height:40,
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              CupertinoPageRoute(builder: (context) => ReferencePage())
+                          ); // forget 페이지로 이동
+                        },
+                      padding: EdgeInsets.all(1),
+                      shape: CircleBorder(),
+                    ),
+
+                    Expanded(
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+
+                              RawMaterialButton(
+                                constraints: BoxConstraints.tight(Size(40, 40)),
+                                onPressed: () {
+                                  Navigator.push(
+                                      context,
+                                      CupertinoPageRoute(builder: (context) => HistoryPage())
+                                  );
+                                },
+                                child: Image(
+                                  image:AssetImage('image/8-layers.png'),
+                                  width:40,
+                                  height:40,
+                                ),
+                                shape: new CircleBorder(),
+                                elevation: 0.0,
+                              ),
+
+                              RawMaterialButton(
+                                constraints: BoxConstraints.tight(Size(40, 40)),
+                                onPressed: (){
+                                },
+                                child: Image(
+                                  image:AssetImage('image/2-layers.png'),
+                                  width:40,
+                                  height:40,
+                                ),
+                                shape: new CircleBorder(),
+                                elevation: 0.0,
+                              ),
+
+
+                              RawMaterialButton(
+                                constraints: BoxConstraints.tight(Size(40, 40)),
+                                onPressed: (){
+                                },
+                                child: Image(
+                                  image:AssetImage('image/Oval (1).png'),
+                                  width:40,
+                                  height:40,
+                                ),
+                                shape: new CircleBorder(),
+                                elevation: 0.0,
+                              ),
+
+                            ]
+                        )
+                    ),
+
+    ],
+                ),
+              ),
+            ),
+
+            Center(
+              child: Container(
+
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+
+
+
+                    Expanded(
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: <Widget>[
+
+                              FlatButton(
+                                child:  Image(
+                                  image:AssetImage('image/Sign_Out_.png'),
+                                  width:71,
+                                  height:17,
+                                ),
+                                onPressed: (){
+                                },
+                              ),
+
+                            ]
+                        )
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            SizedBox(
+              height: 150.0,
+            ),
+
+           TextField(
+
+             maxLines: 5,
+
+             textAlign: TextAlign.center,
+               style: TextStyle(
+                   fontSize: 48.0,
+                   height: 2.0,
+                   color: Color.fromRGBO(60, 60,59,1)
+               ),
+
+             decoration: InputDecoration(
+               border: InputBorder.none,
+               hintText: 'input sentence'
+             ),
+
+           ),
+
+            SizedBox(
+              height: 150.0,
+            ),
+
+          ],
+
+
+
+        ),
+      ),
+
+
+
+
+    )
     );
   }
 }
